9e56b529df97b37d8812b5a21c1adcc580d1b4d8
kimjihye058 <s2321@e-mirim.hs.kr>
2024-01-31 13:58:09 +0900

    계산기능 추가

--- shopping.js
+++ shopping.js
@@ -12,3 +12,17 @@ $(function(){
     
     
  });
+
+ function calculateTotal() {
+   // 수량 가져오기
+   var quantity = document.getElementById('quantity').value;
+
+   // 고정된 상품 가격
+   var fixedPrice = 1200;
+
+   // 총 상품 금액 계산
+   var total = quantity * fixedPrice;
+
+   // 결과 업데이트
+   document.getElementById('price').innerText = total;
+ }
